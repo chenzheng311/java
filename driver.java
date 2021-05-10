b8b434b80426092b5151064066b7ca0291ebe4e5
chenzheng311 <56283283+chenzheng311@users.noreply.github.com>
2021-05-10 22:49:38 +0800

    week10-homework3

--- driver.java
+++ driver.java
@@ -1,65 +1,65 @@
-package homework2;
-import java.util.Scanner;
+package homework3;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 public class driver {
 	public static void main(String[] args) {
-		System.out.println("Please input:");
-		String expression;
-		Scanner scan=new Scanner(System.in);
-		expression = scan.nextLine();
-		String regex = "[a-z]*\\(\\d+[,|，]\\d+\\)";
-		String pattern = "([a-z]*\\(\\d+,([a-z]*\\(\\d+,([a-z]*\\(\\d+,\\d+(\\)\\)\\)))))";
-		Pattern r = Pattern.compile(pattern);
-		Matcher m = r.matcher(expression);
-		double result = 0;
-		if(m.find()) {
-//			System.out.println(m.groupCount());
-//			System.out.println(m.group(1));
-//			System.out.println(m.group(2));
-//			System.out.println(m.group(3));
-//			System.out.println(m.group(4));
-			for(int i = m.groupCount()-1;i>0;i--) {
-				String ex = m.group(i);
-				int x = m.group(i).indexOf(',');
-				String tem = m.group(i).substring(0,x);
-				if(i != m.groupCount()-1)
-					ex = tem+","+result;
-//				System.out.println(ex);
-				result = Caculate(ex);
-				
-				
-			}
-			System.out.println(result);
-		}else if(expression.matches(regex)) {
-			result = Caculate(expression);
-			System.out.println(result);
-		}else {
-			System.out.println("Input Error");
-		}
-		
-		
-	}
-	public static double Caculate(String expression){
-		String[] s = expression.split("\\(|\\)");
-//		System.out.println("Normal");
-		String[] m = s[1].split(",|，");
-//		System.out.println("Normal");
-		double a = Double.parseDouble(m[0]);
-		double b = Double.parseDouble(m[1]);
-		double result=0;
-//		System.out.println("Normal");
-		switch(s[0])
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html><head><title>欢迎访问武汉纺织大学主页</title></head></html>\n");
+		sb.append("<body><img src=’1.jpg’/>\n");
+		sb.append("<a href=’1.htm’>首页</a>\n");
+		sb.append("<a href=’2.htm’>教务处</a>\n");
+		sb.append("<a href=’3.htm’>数计学院</a>\n");
+		sb.append("<img src=’2.jpg’/>\n");
+		sb.append("<img src=’3.jpg’/>\n");
+		sb.append("</body></html>\n");
+		String s = sb.toString();
+		String headregex ="(<.*?><.*?><.*?>(.*(<.*?><.*?><.*?>\\n))).";
+		Pattern r = Pattern.compile(headregex);
+		Matcher m = r.matcher(s);
+		ArrayList<String> srcL=new ArrayList<String>();
+		String regex = "<.*?><.*?><.*?>";
+		if(m.find(0))
 		{
-		case "add":result=a+b;break;
-		case "sub":result=a-b;break;
-		case "max":result = a>b?a:b;break;
-		case "min":result = a<b?a:b;break;
-		default:break;
+		String a =m.group(2).replaceAll(regex, " ");
+		System.out.print("网页标题为：");
+		System.out.println(a);
 		}
-//		System.out.println(result);
-		return result;
+		String SrcRegex="(<a.*?>(.*))";
+		r = Pattern.compile(SrcRegex);
+		m = r.matcher(s);
+		int i = 0;
+		while(m.find())
+		{
+			String sc = m.group();
+			String[] sna = sc.split("<|>");
+//			System.out.println(sna[2]);
+			String[] sr = sc.split("’");
+			srcL.add(sna[2]+"\t"+sr[1]);
+			i++;
+		}
+		System.out.println("网页中共有"+i+"超链接，超链接地址与名称如下：");
+		System.out.println("地址\t名称");
+		for (String string : srcL) {
+			System.out.println(string);
+		}
 		
-		
+		ArrayList<String> imgL=new ArrayList<String>();
+		String imgRegex="(<img.*?>)";
+		r = Pattern.compile(imgRegex);
+		m = r.matcher(s);
+		i =0;
+		while(m.find()) {
+			String ic = m.group();
+			String[] ir = ic.split("’");
+			imgL.add(ir[1]);
+			i++;
+		}
+		System.out.println();
+		System.out.print("网页中共有"+i+"个图片，图片名称为：");
+		for (String string : imgL) {
+			System.out.print(string+"\t");
+		}
 	}
 }
